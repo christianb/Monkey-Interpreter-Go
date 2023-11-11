4f07c70e70588e2744ce23a1c12e168229e88c31
Christian Bunk <christian.bunk@nordsec.com>
2023-11-11 20:39:34 +0100

    Implementing the Pratt Parser - Read-Parse-Print-Loop (p:126-129)

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ This is an interpreter written in GoLang for the Monkey programming language.
 All code and samples are based on the book ["Writing An Interpreter in Go"](https://interpreterbook.com) by Thorsten Ball.
 
 ### Run Tests
-To execute all tests in all packages run: `go test ./...`
+To execute all tests in all packages: `go test ./...`
+
+### Run the REPL
+To start the REPL: `go run main.go`
 
 ### About The Monkey Programming Language
 * [https://interpreterbook.com](https//interpreterbook.com)
--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -21,10 +21,38 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		l := lexer.New(line)
+		lexer := lexer.New(line)
+		parser := parser.New(lexer)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := parser.ParseProgram()
+		if len(parser.Errors()) != 0 {
+			printParserErrors(out, parser.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
