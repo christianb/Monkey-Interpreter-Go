24e7e47b01d0ef52f58ef0d17548a43357cdba72
Christian Bunk <christian.bunk@nordsec.com>
2024-01-07 19:52:58 +0100

    Evaluation - Bindings & The Environment (p:172-177)

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -38,7 +38,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	program := parser.New(l).ParseProgram()
-	return Eval(program)
+	env := object.NewEnvironment()
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
@@ -190,7 +191,6 @@ func TestErrorHandling(t *testing.T) {
 		input           string
 		expectedMessage string
 	}{
-
 		{
 			"5 + true;",
 			"type mismatch: INTEGER + BOOLEAN",
@@ -216,15 +216,19 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{`
-			if (10 > 1) {
 				if (10 > 1) {
-					return true + false;
-				}
+					if (10 > 1) {
+						return true + false;
+					}
 
-				return 1;
-			}
-			`,
+					return 1;
+				}
+				`,
 			"unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			"foobar",
+			"identifier not found: foobar",
 		},
 	}
 
@@ -240,5 +244,21 @@ func TestErrorHandling(t *testing.T) {
 		if errObj.Message != tt.expectedMessage {
 			t.Errorf("wrong error message. expected+%q, got=%q", tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
